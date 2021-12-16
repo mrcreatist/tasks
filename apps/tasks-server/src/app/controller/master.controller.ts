3146c42e5076eb74f04478a1019e6fc11df509d7
Abhishek Verma <ahk.verma102@gmail.com>
2021-12-16 16:13:59 +0530

    added changes

--- apps/tasks-server/src/app/controller/master.controller.ts
+++ apps/tasks-server/src/app/controller/master.controller.ts
@@ -1,8 +1,5 @@
 export const master = {
     welcome: (req, res) => {
         res.send({ message: 'Welcome to server!' })
-    },
-    getMenu: (req, res) => {
-        res.send({ message: 'get-menu works!' })
-    },
+    }
 }
--- apps/tasks-server/src/main.ts
+++ apps/tasks-server/src/main.ts
@@ -1,8 +1,47 @@
 import * as routes from './app/route'
 import * as express from 'express';
+import * as cors from 'cors';
+import * as socketIO from 'socket.io';
+import * as fs from 'fs';
 
 const app = express();
 app.use(express.json());
+
+// const io = socketIO(app);
+const file = 'task-data.json';
+
+app.use(cors());
+
+
+function writeDataToFile(dataStore) {
+  const data = JSON.stringify(dataStore);
+  if (data && data.length) {
+    fs.writeFileSync(file, data);
+  }
+}
+
+function readDataFromFile() {
+  if (!fs.existsSync(file)) {
+    writeDataToFile(null);
+  }
+  const rawData = fs.readFileSync(file);
+  return JSON.parse(rawData.toString());
+}
+
+
+io.on('connection', (socket) => {
+  console.log('welcome', socket.id);
+  socket.emit('fireInTheHole', readDataFromFile());
+
+  socket.on('fireInTheHole', () => console.log('fireInTheHole event triggered'));
+  socket.on('makeFireInTheHole', (data) => {
+    writeDataToFile(data);
+    socket.broadcast.emit('fireInTheHole', readDataFromFile());
+  });
+  socket.on('disconnect', () => console.log('disconnected from server'));
+});
+
+
 
 routes.register(app);
 
@@ -15,40 +54,8 @@ server.on('error', console.error);
 
 
 
-// const app = require('express')();
-// const cors = require('cors');
-// let server = require('http').createServer(app);
-// let io = require('socket.io')(server);
-// const fs = require('fs');
-// const file = 'task-data.json';
 
-// app.use(cors());
 
-// function writeDataToFile(dataStore) {
-//   let data = JSON.stringify(dataStore);
-//   if (data && data.length) {
-//     fs.writeFileSync(file, data);
-//   }
-// }
 
-// function readDataFromFile() {
-//   if (!fs.existsSync(file)) {
-//     writeDataToFile(null);
-//   }
-//   let rawData = fs.readFileSync(file);
-//   return JSON.parse(rawData);
-// }
-
-// io.on('connection', (socket) => {
-//   console.log('welcome', socket.id);
-//   socket.emit('fireInTheHole', readDataFromFile());
-
-//   socket.on('fireInTheHole', () => console.log('fireInTheHole event triggered'));
-//   socket.on('makeFireInTheHole', (data) => {
-//     writeDataToFile(data);
-//     socket.broadcast.emit('fireInTheHole', readDataFromFile());
-//   });
-//   socket.on('disconnect', () => console.log('disconnected from server'));
-// });
 
 // server.listen(process.env.PORT || 3333, () => console.log(`listening on ${process.env.PORT || 3333}`));
